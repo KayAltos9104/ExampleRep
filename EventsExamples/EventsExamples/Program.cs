03d611d53428947d42f76b0739db7458c380f5c2
Kay <kayaltosnexus@gmail.com>
2022-02-14 18:34:09 +0300

    -Initial

---
+++ EventsExamples/EventsExamples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EventsExamples
+{
+    class Program
+    {
+        public delegate void Message();
+
+        public static event Message MessageHandler;
+
+
+        static void Main(string[] args)
+        {
+
+            //Message mes;
+            //mes = GoodMorning;
+            //mes += GoodEvening;
+            //mes();
+
+
+            MessageHandler += GoodMorning;
+            MessageHandler += GoodEvening;
+
+            MessageHandler();
+        }
+        static void GoodMorning()
+        {
+            Console.WriteLine("Good morning!");
+        }
+        static void GoodEvening()
+        {
+            Console.WriteLine("Good evening!");
+        }
+    }
+}
---
+++ EventsExamples/Example2(Args)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example2_Args_
+{
+    class Program
+    {
+        static void Main()
+        {
+            EventsExample e1 = new EventsExample();
+            e1.MathActionHandler += Sum;//Кто (какой метод) подписывается на событие и, соответственно, реагирует на него
+            e1.MathActionHandler += Multiple;
+            e1.InputNumbers();
+            
+
+        }
+
+        static void Sum (object sender, MathActionEventArgs e)
+        {
+            Console.WriteLine($"Сложили {e.A} и {e.B}. Получилось {e.A+e.B}");
+        }
+
+        static void Multiple(object sender, MathActionEventArgs e)
+        {
+            Console.WriteLine($"Умножали {e.A} и {e.B}. Получилось {e.A * e.B}");
+        }
+    }
+
+    class EventsExample
+    {
+        public delegate void MathAction (object sender, MathActionEventArgs e);
+        public event MathAction MathActionHandler;
+
+        public void InputNumbers ()
+        {
+            MathActionHandler.Invoke(this/*Кто вызвал событие*/, new MathActionEventArgs(4, 5)/*Класс с аргументами события*/);
+        }
+    }
+
+    class MathActionEventArgs
+    {
+        public int A { get; }
+        public int B { get; }
+
+        public MathActionEventArgs(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+    } 
+}
